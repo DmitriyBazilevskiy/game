cea177b6a67bebcabefbbac33a72d5dc08ebaa9f
DmitriyBazilevskiy <dima17291195@gmail.com>
2022-03-26 12:08:42 +0200

    game

--- run.js
+++ run.js
@@ -10,19 +10,19 @@ var jsonParser = bodyParser.json();
 
 let schedule = {};
 
-console.log( isEmpty(schedule) ); // true
+console.log(isEmpty(schedule)); // true
 
 schedule["8:30"] = "get up";
 
-console.log( isEmpty(schedule) ); // false
+console.log(isEmpty(schedule)); // false
 
 function isEmpty(obg) {
-  var result = false;
-  for (key in obg) {
-      result = true;
-      break;
-  }
-  return result;
+    var result = false;
+    for (key in obg) {
+        result = true;
+        break;
+    }
+    return result;
 }
 
 // app.use(
@@ -36,37 +36,37 @@ app.use('/game', express.static(__dirname));
 
 //app.get('/game/best_time.json', (req, res) => res.json({ time: '100'}));
 
-app.get('/game/best_time.json', function(req, res) {
+app.get('/game/best_time.json', function (req, res) {
 
-  var sqlite3 = require('sqlite3').verbose();
-  var db = new sqlite3.Database('base.sqlite');
+    var sqlite3 = require('sqlite3').verbose();
+    var db = new sqlite3.Database('base.sqlite');
 
-  db.all("SELECT time FROM best_time", function(err, rows) {
+    db.all("SELECT time FROM best_time", function (err, rows) {
 
         //  console.log(err);
 
-          if (rows == undefined) {
-              time = 0;
-              console.log('ploxo');
-          } else {
-              if (rows.length < 1) {
-                  time = 0;
-              }
-              rows.forEach(function (row) {
-                  console.log(row.time);
-                  time = row.time;
-              });
-          };
+        if (rows == undefined) {
+            time = 0;
+            console.log('ploxo');
+        } else {
+            if (rows.length < 1) {
+                time = 0;
+            }
+            rows.forEach(function (row) {
+                console.log(row.time);
+                time = row.time;
+            });
+        };
 
-          db.close();
+        db.close();
 
-          res.json({ time: `${time}`});
+        res.json({ time: `${time}` });
 
-  	});
+    });
 
 });
 
-app.post('/game/insert_best_time.json', jsonParser, function(req, res) {
+app.post('/game/insert_best_time.json', jsonParser, function (req, res) {
 
     var sqlite3 = require('sqlite3').verbose();
     var db = new sqlite3.Database('base.sqlite');
@@ -74,7 +74,7 @@ app.post('/game/insert_best_time.json', jsonParser, function(req, res) {
     //console.log(req.body.value);
     var time = req.body.value;
 
-    db.all(`INSERT into best_time(time)values(${time})`, function(err) {
+    db.all(`INSERT into best_time(time)values(${time})`, function (err) {
 
         db.close();
 
@@ -91,7 +91,7 @@ app.post('/game/insert_best_time.json', jsonParser, function(req, res) {
 
 });
 
-app.post('/game/update_best_time.json', jsonParser, function(req, res) {
+app.post('/game/update_best_time.json', jsonParser, function (req, res) {
 
     var sqlite3 = require('sqlite3').verbose();
     var db = new sqlite3.Database('base.sqlite');
@@ -99,7 +99,7 @@ app.post('/game/update_best_time.json', jsonParser, function(req, res) {
     //console.log(req.body.value);
     var time = req.body.value;
 
-    db.all(`UPDATE best_time SET time = ${time}`, function(err) {
+    db.all(`UPDATE best_time SET time = ${time}`, function (err) {
 
         db.close();
 
@@ -125,5 +125,5 @@ app.post('/game/update_best_time.json', jsonParser, function(req, res) {
 // });
 
 app.listen(port, host, function () {
-  console.log(`Server listens http://${host}:${port}`);
+    console.log(`Server listens http://${host}:${port}`);
 });
